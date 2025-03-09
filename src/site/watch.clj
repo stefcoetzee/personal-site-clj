b1e28b3ef4d1c90c1d852f1e596a9e0e436b41d4
Stef Coetzee <stefcoetzee@protonmail.com>
2025-03-09 11:27:26 -0400

    Remove deprecated namespaces

--- src/site/watch.clj
+++
@@ -1,9 +0,0 @@
-(ns site.watch
-  (:require [site.core]
-            [site.server :as server]
-            [dev.server.hot-reload :refer [dev-start!]]))
-
-(defn start! []
-  (site.core/build!)
-  (server/static-start!)
-  (dev-start!))
